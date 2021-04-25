43d1c3b4e5c8a92ec9658fa72435422b917e0d53
hee-c <ways2kim@gmail.com>
2021-04-25 17:56:34 +0900

    Add login page, form

---
+++ src/pages/Login.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ipcRenderer } from 'electron';
+import LoginForm from '../components/LoginForm.jsx';
+import { login } from '../apis/index.js';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const FormWrapper = styled.div`
+  display: flex;
+  width: 40%;
+  height: 100%;
+`;
+
+const ImageWrapper = styled.div`
+  display: flex;
+  width: 60%;
+  height: 100%;
+`;
+
+const Img = styled.img`
+  background-color: yellow;
+`;
+
+export default function Login() {
+  async function handleLoginSubmit(data) {
+    try {
+      const response = await login(data);
+      window.location.hash = '#/alarmRegister';
+      // ipcRenderer.send('storeUserData', data);
+    } catch (error) {
+      window.location.hash = '#/login';
+    }
+  }
+
+  return (
+    <Container>
+      <FormWrapper>
+        <LoginForm onLoginSubmit={handleLoginSubmit}/>
+      </FormWrapper>
+      <ImageWrapper>
+        <Img src="" alt=""/>
+      </ImageWrapper>
+    </Container>
+  );
+}
